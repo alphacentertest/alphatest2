f9e9314d3e889ec0c25827178c0e2172b9e23ebd
alphacentertest <alphacentertest@gmail.com>
2025-04-05 12:08:04 +0300

    Update logger.js to use console transport for Vercel

--- logger.js
+++ logger.js
@@ -1,10 +1,5 @@
 // logger.js
 const winston = require('winston');
-const path = require('path');
-const fs = require('fs');
-
-// Используем /tmp для хранения логов
-const logDir = '/tmp/logs';
 
 const logger = winston.createLogger({
   level: 'info',
@@ -13,15 +8,8 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
   transports: [
-    new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
-    new winston.transports.File({ filename: path.join(logDir, 'combined.log') }),
-    new winston.transports.Console(),
+    new winston.transports.Console(), // Логи только в консоль
   ],
 });
-
-// Создаём директорию /tmp/logs, если она не существует
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir, { recursive: true });
-}
 
 module.exports = logger;
