dc4dac8902f1417d5f094784e847ec5f5f89e730
alphacentertest <alphacentertest@gmail.com>
2025-04-07 12:49:49 +0300

    Add logger.js to fix TypeError in server.js

--- logger.js
+++ logger.js
@@ -1,3 +1,4 @@
+// logger.js
 const winston = require('winston');
 
 const logger = winston.createLogger({
@@ -7,10 +8,11 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
     new winston.transports.Console(),
-  ],
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
 });
 
+// Ensure the logger is exported using CommonJS
 module.exports = logger;
